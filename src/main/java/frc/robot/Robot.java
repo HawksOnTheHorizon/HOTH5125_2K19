26fbab82cdda51982f9a5d157c984763b3ddbff9
IgnisVAqua <greenwolf121@gmail.com>
2019-01-19 14:12:00 -0600

    Salia first commit robodrive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,14 +1,34 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.SpeedControllerGroup; 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends TimedRobot {
-
+  VictorSP tr = new VictorSP(0); 
+  VictorSP tl = new VictorSP(1);
+  VictorSP br = new VictorSP(2); 
+  VictorSP bl = new VictorSP(3);
   
+  Joystick lJoystick = new Joystick(0);
+  Joystick rJoystick = new Joystick(1);
+ 
+  SpeedControllerGroup right = new SpeedControllerGroup(tr, br);
+  SpeedControllerGroup left = new SpeedControllerGroup(tl, bl);
+   
+  DifferentialDrive hothBot = new DifferentialDrive(left,right);
   @Override
   public void robotInit() {
  // Initialize all components for Teleop
-  }
+ 
+}
 
   @Override
   public void autonomousInit() {
@@ -22,6 +42,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    
  // Write Teleop code  
+    hothBot.tankDrive(lJoystick.getY(), rJoystick.getY());
   }
 }
