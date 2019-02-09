c6b5f77f66d1e5648d292a4654ec61057b3e0113
Chemistry 4 <hothfrc5125@gmail.com>
2019-02-08 18:57:49 -0600

    testing this f*king code :)))))) plz work

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,15 +1,32 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.SpeedControllerGroup; 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class Robot extends TimedRobot {
 
-  
+  VictorSP leftW = new VictorSP(0);
+  VictorSP rightW = new VictorSP(1);
+  VictorSP arm = new VictorSP(2);
+  Joystick contr = new Joystick(0);
+  JoystickButton a =new JoystickButton(contr, 1);
+  JoystickButton b = new JoystickButton(contr, 2);
+  JoystickButton x = new JoystickButton(contr, 3);  
+  JoystickButton y = new JoystickButton(contr, 3);
+
   @Override
   public void robotInit() {
  // Initialize all components for Teleop
-    int i =10;
-    double test = 1.10;
+   
   }
 
   @Override
@@ -24,6 +41,29 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
- // Write Teleop code  
+    if(y.get()){
+      arm.set(0.25);
+    }else{
+      arm.set(0.0);
+    }
+
+    if(a.get()){
+      arm.set(-0.25);
+    }else{
+      arm.set(0.0);
+    }
+
+    if(x.get()){
+    rightW.set(1.0);
+    leftW.set(-1.0);
+  }else if(b.get()){
+    rightW.set(-1.0);
+    leftW.set(1.0);
+  }else{
+    rightW.set(0.0);
+    leftW.set(0.0);
   }
+
+
+
 }
