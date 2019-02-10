4507a5b2c354f095579991e265cb7e0709192fc6
Chemistry 4 <hothfrc5125@gmail.com>
2019-02-10 17:30:00 -0600

    test victor+talons

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,17 +11,25 @@ import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.SpeedControllerGroup; 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;;
+
 
 public class Robot extends TimedRobot {
 
-  VictorSP leftW = new VictorSP(0);
-  VictorSP rightW = new VictorSP(1);
-  VictorSP arm = new VictorSP(2);
+  // VictorSP leftW = new VictorSP(0);
+  // VictorSP rightW = new VictorSP(1);
+  // VictorSP arm = new VictorSP(2);
   Joystick contr = new Joystick(0);
   JoystickButton a =new JoystickButton(contr, 1);
   JoystickButton b = new JoystickButton(contr, 2);
   JoystickButton x = new JoystickButton(contr, 3);  
   JoystickButton y = new JoystickButton(contr, 4);
+  JoystickButton rButton = new JoystickButton(contr, 6);
+
+  TalonSRX rightW = new TalonSRX(1);
+  VictorSPX leftW = new VictorSPX(6);
 
   @Override
   public void robotInit() {
@@ -41,27 +49,33 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    // if(x.get()){
+    //   test.set(ControlMode.PercentOutput , 1);
+
+    // }else{
+    //   test.set(ControlMode.PercentOutput , 0);
+    // }
+
+    // if(b.get()){
+    //   arm.set(ControlMode.PercentOutput , .25);
+    // }else{
+    //   arm.set(ControlMode.PercentOutput , 0);
+    // }
+
+    // if(y.get()){
+    //   arm.set(ControlMode.PercentOutput , -.25);
+    // }else{
+    //   arm.set(ControlMode.PercentOutput , 0);
+    // }
     if(x.get()){
-      arm.set(0.25);
-    }else{
-      arm.set(0.0);
-    }
-
-    if(b.get()){
-      arm.set(-0.25);
-    }else{
-      arm.set(0.0);
-    }
-
-  //   if(x.get()){
-  //   rightW.set(1.0);
-  //   leftW.set(-1.0);
-  // }else if(b.get()){
-  //   rightW.set(-1.0);
-  //   leftW.set(1.0);
-  // }else{
-  //   rightW.set(0.0);
-  //   leftW.set(0.0);
-//  }
+    rightW.set(ControlMode.PercentOutput ,-1);
+    leftW.set(ControlMode.PercentOutput ,1);
+  }else if(b.get()){
+    rightW.set(ControlMode.PercentOutput ,1);
+    leftW.set(ControlMode.PercentOutput ,-1);
+  }else{
+    rightW.set(ControlMode.PercentOutput,0);
+    leftW.set(ControlMode.PercentOutput,0);
+ }
 }
 }
