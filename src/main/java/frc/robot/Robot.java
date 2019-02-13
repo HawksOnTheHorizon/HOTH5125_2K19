b9eda34e88baaf7251bb829546a18366d1e8ae04
Chemistry 4 <hothfrc5125@gmail.com>
2019-02-13 13:00:46 -0600

    Changed arm code Added vicotors

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.SpeedControllerGroup; 
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -18,74 +18,65 @@ import edu.wpi.first.wpilibj.Servo;
 
 public class Robot extends TimedRobot {
 
-  // VictorSP leftW = new VictorSP(0);
-  // VictorSP rightW = new VictorSP(1);
-  // VictorSP arm = new VictorSP(2);
-  Servo finger = new Servo(0); 
+  Servo finger = new Servo(0);
   Joystick contr = new Joystick(0);
-  JoystickButton a =new JoystickButton(contr, 1);
+  JoystickButton a = new JoystickButton(contr, 1);
   JoystickButton b = new JoystickButton(contr, 2);
-  JoystickButton x = new JoystickButton(contr, 3);  
+  JoystickButton x = new JoystickButton(contr, 3);
   JoystickButton y = new JoystickButton(contr, 4);
-  JoystickButton rButton = new JoystickButton(contr, 6);
+  JoystickButton rightB = new JoystickButton(contr, 5);
 
   TalonSRX rightW = new TalonSRX(1);
-  VictorSPX leftW = new VictorSPX(6);
+  TalonSRX leftW = new TalonSRX(2);
+  TalonSRX arm = new TalonSRX(6);
 
   @Override
   public void robotInit() {
- // Initialize all components for Teleop
-   
+    // Initialize all components for Teleop
+
   }
 
   @Override
   public void autonomousInit() {
- // Initialize all components for Autonomous
+    // Initialize all components for Autonomous
   }
 
   @Override
   public void autonomousPeriodic() {
- // Write Autnomous code
+    // Write Autnomous code
   }
 
   @Override
   public void teleopPeriodic() {
-    // if(x.get()){
-    //   test.set(ControlMode.PercentOutput , 1);
 
-    // }else{
-    //   test.set(ControlMode.PercentOutput , 0);
-    // }
+    if (a.get()) {
+      arm.set(ControlMode.PercentOutput, .25);
+    } else {
+      arm.set(ControlMode.PercentOutput, 0);
+    }
 
-    // if(b.get()){
-    //   arm.set(ControlMode.PercentOutput , .25);
-    // }else{
-    //   arm.set(ControlMode.PercentOutput , 0);
-    // }
+    if (y.get()) {
+      arm.set(ControlMode.PercentOutput, -.25);
+    } else {
+      arm.set(ControlMode.PercentOutput, 0);
+    }
 
-    // if(y.get()){
-    //   arm.set(ControlMode.PercentOutput , -.25);
-    // }else{
-    //   arm.set(ControlMode.PercentOutput , 0);
-    // }
-    if(x.get()){
-    rightW.set(ControlMode.PercentOutput ,-1);
-    leftW.set(ControlMode.PercentOutput ,1);
-  }else if(b.get()){
-    rightW.set(ControlMode.PercentOutput ,1);
-    leftW.set(ControlMode.PercentOutput ,-1);
-  }else{
-    rightW.set(ControlMode.PercentOutput,0);
-    leftW.set(ControlMode.PercentOutput,0);
- }
+    if (x.get()) {
+      rightW.set(ControlMode.PercentOutput, -1);
+      leftW.set(ControlMode.PercentOutput, 1);
+    } else if (b.get()) {
+      rightW.set(ControlMode.PercentOutput, 1);
+      leftW.set(ControlMode.PercentOutput, -1);
+    } else {
+      rightW.set(ControlMode.PercentOutput, 0);
+      leftW.set(ControlMode.PercentOutput, 0);
+    }
 
+    if (y.get()) {
+      finger.setAngle(150);
+    } else {
+      finger.setAngle(70);
+    }
 
- if (y.get()) {
-   finger.setAngle(150);
- }
- else {
-   finger.setAngle(70);
- }
-
-}
+  }
 }
