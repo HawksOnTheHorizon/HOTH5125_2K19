8df4123f953bbb798498a4ed8e7df8a3bf6f80f7
Chemistry 4 <hothfrc5125@gmail.com>
2019-02-15 16:03:56 -0600

    added secondary cam

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,29 +1,68 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
+
 public class Robot extends TimedRobot {
 
-  
   @Override
   public void robotInit() {
- // Initialize all components for Teleop
-CameraServer.getInstance().startAutomaticCapture();
-}
+    // Initialize all components for Teleop
+    
+    new Thread(() -> {
+      UsbCamera camera1 = CameraServer.getInstance().startAutomaticCapture(0);
+      camera1.setResolution(640, 480);
 
+      CvSink cvSink = CameraServer.getInstance().getVideo();
+      CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+
+      Mat source = new Mat();
+      Mat output = new Mat();
+
+      while (!Thread.interrupted()) {
+        cvSink.grabFrame(source);
+        Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+        outputStream.putFrame(output);
+      }
+    }).start();
+
+    new Thread(() -> {
+      UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture(1);
+      camera2.setResolution(640, 480);
+
+      CvSink cvSink = CameraServer.getInstance().getVideo();
+      CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+
+      Mat source = new Mat();
+      Mat output = new Mat();
+
+      while (!Thread.interrupted()) {
+        cvSink.grabFrame(source);
+        Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+        outputStream.putFrame(output);
+      }
+    }).start();
+    
+  }
 
   @Override
   public void autonomousInit() {
- // Initialize all components for Autonomous
+    // Initialize all components for Autonomous
   }
 
   @Override
   public void autonomousPeriodic() {
- // Write Autnomous code
+    // Write Autnomous code
   }
 
   @Override
   public void teleopPeriodic() {
- // Write Teleop code  
+    // Write Teleop code
   }
 }
