06c03917977406944c1e33d3d44ee45714317b2e
Chemistry 4 <hothfrc5125@gmail.com>
2019-02-16 12:37:50 -0600

    adddded olenoid

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,31 +1,49 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class Robot extends TimedRobot {
 
   Compressor c = new Compressor(0);
+  DoubleSolenoid beakThing = new DoubleSolenoid(1, 0);
   
+  Joystick cont = new Joystick(0);
+
+  JoystickButton a = new JoystickButton(cont, 1);
+  JoystickButton b = new JoystickButton(cont, 2);
+  JoystickButton x = new JoystickButton(cont, 3);
+
   @Override
   public void robotInit() {
- // Initialize all components for Teleop
- c.setClosedLoopControl(true);
- 
+    // Initialize all components for Teleop
+    c.setClosedLoopControl(true);
+
   }
 
   @Override
   public void autonomousInit() {
- // Initialize all components for Autonomous
+    // Initialize all components for Autonomous
   }
 
   @Override
   public void autonomousPeriodic() {
- // Write Autnomous code
+    // Write Autnomous code
   }
 
   @Override
   public void teleopPeriodic() {
- // Write Teleop code  
+    // Write Teleop code
+    if(a.get()){
+      beakThing.set(DoubleSolenoid.Value.kForward);
+    }else{
+      beakThing.set(DoubleSolenoid.Value.kOff);
+    }
+
   }
 }
