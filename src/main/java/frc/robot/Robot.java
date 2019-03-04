9df280dabc99455acaf401f7fc0f627f26fe936f
Chemistry 4 <hothfrc5125@gmail.com>
2019-03-03 20:27:34 -0600

    Ultrasoinc code too

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,14 +3,17 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Ultrasonic;
 
 public class Robot extends TimedRobot {
   DigitalInput lightfollwer;
+  Ultrasonic ultra = new Ultrasonic(5, 6);
 
   @Override
   public void robotInit() {
     // Initialize all components for Teleop
-    lightfollwer = new DigitalInput(1);
+    lightfollwer = new DigitalInput   (4);
+    ultra.setAutomaticMode(true);
   }
 
   @Override
@@ -25,6 +28,8 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    System.out.println(lightfollwer.get());
+    //System.out.println(lightfollwer.get());
+    System.out.println(ultra.getRangeInches());
+
   }
 }
